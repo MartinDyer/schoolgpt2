2ea37229e6af2e352ad3815fe4f03c647d5e40a9
GitHub Action <action@github.com>
2025-09-22 13:52:10 +0000

    🏫 Configure SchoolGPT for Blundells (production)

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "blundell-production-rg"
-    storage_account_name = "blundelltfstatea8dcb8"
+    storage_account_name = "blundelltfstate6db415"
     container_name       = "tfstate"
     key                  = "production.terraform.tfstate"
   }
