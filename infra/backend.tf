8bf249cf04090f6a6db74388e6571781e313299a
GitHub Action <action@github.com>
2025-11-07 16:00:17 +0000

    🏫 Configure SchoolGPT for Luke-Test (production)

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Management"
-    storage_account_name = "tfstatelax"
+    resource_group_name  = "luketest-production-rg"
+    storage_account_name = "luketesttfstate471e78"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "production.terraform.tfstate"
   }
 }
