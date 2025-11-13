d9b4c89a5a175e6a037a7914f53544bdaacf73fe
Luke Axon <lukeaxon1@gmail.com>
2025-11-13 12:09:40 +0000

    Update tf variables

--- infra/main.tf
+++ infra/main.tf
@@ -183,10 +183,9 @@ resource "azurerm_linux_web_app" "backend" {
 
 resource "azurerm_linux_web_app" "frontend" {
   name                = var.frontend_app_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  service_plan_id     = azurerm_service_plan.this.id
-
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  service_plan_id     = azurerm_service_plan.main.id
   https_only = true
 
   site_config {
