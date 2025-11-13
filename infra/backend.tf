919916dcd300c174d971ff2f37e6a23b027beb0a
GitHub Action <action@github.com>
2025-11-13 14:11:08 +0000

    🏫 Configure SchoolGPT for testscho (production)

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "blundell-production-rg"
-    storage_account_name = "blundelltfstate041a21"
+    resource_group_name  = "testscho-production-rg"
+    storage_account_name = "testschotfstate028a08"
     container_name       = "tfstate"
     key                  = "production.terraform.tfstate"
   }
