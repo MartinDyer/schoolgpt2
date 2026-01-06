cd45b3c58edb44904f8b2025912abdf80b5d8c6f
Muhammad Umair Ali <muhammadumairali@MacBook-Pro.local>
2026-01-06 21:09:55 +0500

    fix(infra): allow Azure services to access SQL server

--- infra/main.tf
+++ infra/main.tf
@@ -328,6 +328,14 @@ resource "azurerm_mssql_database" "main" {
   }
 }
 
+# Allow Azure Services (including App Service) to access SQL Server
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_mssql_server.main.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
 # Build SQL connection string
 locals {
   sql_connection_string = "Server=tcp:${azurerm_mssql_server.main.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.main.name};Persist Security Info=False;User ID=${var.sql_admin};Password=${local.effective_sql_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
