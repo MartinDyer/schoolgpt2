fe7b080f5e524c873ca10bbfc53c08c846a3c92e
Muhammad Umair Ali <muhammadumairali@MacBook-Pro.local>
2026-01-07 22:12:19 +0500

    fix: allow force deletion of resource groups in terraform provider

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,12 @@
 
 # Provider
+# Provider
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "random_string" "unique" {
