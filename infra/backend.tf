f5b787a5ac75c6c5962144aac76fe45f9e054f09
GitHub Action <action@github.com>
2026-01-07 17:25:38 +0000

    🏫 Configure SchoolGPT for test1 (production)

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "school1-production-rg"
-    storage_account_name = "school1tfstatecc1a24"
+    resource_group_name  = "test1-production-rg"
+    storage_account_name = "test1tfstate5b30f7"
     container_name       = "tfstate"
     key                  = "production.terraform.tfstate"
   }
